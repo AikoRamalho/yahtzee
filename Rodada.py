a90a6bad314d72c2bf090a44119b27273d36d368
Aiko Ramalho <aiko.ramalho@voaed.com>
2020-04-24 15:07:42 -0300

    adiciona modulo rodada e completa o arquivo testes

---
+++ Rodada.py
@@ -0,0 +1,88 @@
+#Versao 1.0.7
+#Ultimo modificacao: Aiko Ramalho
+import Dados
+
+MAX_RODADAS = 26 #define o numero maximo de rodadas (numero padrao de um jogo yahtzee)
+
+__all__ = ["Cria_Rodada", "Verifica_Tentativa", "Atualiza_Tentativas", "Modifica_Dados_Rodada", "Deleta_Rodadas", "Pega_Rodada"]
+
+rodadas = []
+
+#funcao responsavel por criar o objeto rodada com 3 tentativas
+#retorna 0 no sucesso
+#retorna 1 caso numero de rodadas maxima tenha sido atingida
+#retorna 2 caso os dados da rodada anterior n tenham sido deletados
+def Cria_Rodada():
+    numero_rodada = len(rodadas)
+    retorno_mostra_dados = Dados.Mostra_Dados()
+    if(numero_rodada > MAX_RODADAS):
+        return 1
+    if(list(retorno_mostra_dados.values())[0] == []):
+        nova_rodada = {numero_rodada+1: {"tentativas": 3, "dados_rodada": []}}
+        rodadas.append(nova_rodada)
+        return 0
+    return 2
+
+#funcao responsavel por verificar se ainda restam tentativas na ultima rodada criada
+#retorna 0 caso tenham tentativas
+#retorna 1 caso nao tenham tentativas
+#retorna 2 caso nao tenham rodadas
+def Verifica_Tentativa():
+    numero_rodada = len(rodadas)
+    if(numero_rodada == 0):
+        return 2
+    num_tentativas = rodadas[numero_rodada-1][numero_rodada]["tentativas"]
+    if(num_tentativas == 0):
+        return 1
+    return 0
+
+#funcao responsavel por atualizar o numero de tentativas da ultima rodada
+#retorna 0 caso sucesso
+#retorn 1 caso o numero de tentativas do parametro n seja num_tentativas-1 ou zero
+#retorna 2 caso o numero passado n esteja no range de tentativas
+#retorn 3 caso o numero passado n seja int
+#retorna 4 caso nao tenham rodadas
+def Atualiza_Tentativas(num):
+    numero_rodada = len(rodadas)
+    if(type(num) != type(0)):
+        return 3
+    if(num not in range(0, 4)): #range no python eh aberto no extremo da direita
+        return 2
+    if(numero_rodada == 0):
+        return 4
+    num_tentativas = rodadas[numero_rodada-1][numero_rodada]["tentativas"]
+    if(num != 0 and num != num_tentativas-1):
+        return 1
+    rodadas[numero_rodada-1][numero_rodada]["tentativas"] = num
+    return 0
+
+#funcao responsavel por atualizar os dados da ultima rodada
+#retorna 0 caso sucesso
+#retorn 1 caso nao restam tentativas na rodada
+#retorna 2 caso todos os dados estejam congelados
+#retorn 3 caso nao hajam dados
+#retorna 4 caso nao tenham rodadas
+def Modifica_Dados_Rodada():
+    numero_rodada = len(rodadas)
+    if(numero_rodada == 0):
+        return 4
+    num_tentativas = rodadas[numero_rodada-1][numero_rodada]["tentativas"]
+    if(num_tentativas == 0): 
+        return 1
+    retorno_dados = Dados.Jogar_Dados()
+    if(retorno_dados == 1):
+        return 3
+    if(retorno_dados == 2):
+        return 2
+    dados = Dados.Mostra_Dados()
+    rodadas[numero_rodada-1][numero_rodada]["dados_rodada"] = list(dados.values())[0]
+    return 0
+
+def Deleta_Rodadas():
+    rodadas.clear()
+
+def Pega_Rodada():
+    numero_rodada = len(rodadas)
+    if(numero_rodada == 0):
+        return {1: None}
+    return{0: rodadas[numero_rodada-1]}
--- Teste.py
+++ Teste.py
@@ -1,6 +1,7 @@
 import unittest
 from Jogador import Cria_Novo_Jogador, Destroi_Jogadores, Pega_Jogadores
 from Dados import Cria_Dados, Muda_Status, Mostra_Dados, Destroi_Dados, Jogar_Dados
+from Rodada import Cria_Rodada, Verifica_Tentativa, Atualiza_Tentativas, Modifica_Dados_Rodada, Deleta_Rodadas, Pega_Rodada
 class Test(unittest.TestCase):    
 
     #Modulo Jogador
@@ -11,7 +12,7 @@ class Test(unittest.TestCase):
         self.assertEqual(retorno_esperado, 0)
 
     def testJogador_02_CriaNovoJogador_Ja_Existem_Jogadores(self): 
-        print("Caso de Teste Jogador 02 - Já existem dois jogadores cadastrados")
+        print("Caso de Teste Jogador 02 - Ja existem dois jogadores cadastrados")
         Destroi_Jogadores()
         Cria_Novo_Jogador('aiko')
         Cria_Novo_Jogador('carlos')
@@ -53,21 +54,21 @@ class Test(unittest.TestCase):
         self.assertEqual(retorno_esperado,0)
 
     def testDados_02_CriaDados_Ja_Existem_Dados_Criados(self): 
-        print("Caso de Teste Dados 02 - Já existem dados criados")
+        print("Caso de Teste Dados 02 - Ja existem dados criados")
         Destroi_Dados()
         Cria_Dados()
         retorno_esperado = Cria_Dados()
         self.assertEqual(retorno_esperado,1)
 
     def testDados_03_MudaStatus_Ok_Condicao_Retorno_1(self): 
-        print("Caso de Teste Dados 03 - Mudança de estado do dado 1 com sucesso")
+        print("Caso de Teste Dados 03 - Mudanca de estado do dado 1 com sucesso")
         Destroi_Dados()
         Cria_Dados()
         retorno_esperado = Muda_Status(1)
         self.assertEqual(retorno_esperado,0)
 
     def testDados_04_MudaStatus_Nao_Existe_Dado(self): 
-        print("Caso de Teste Dados 04 - Esse ID não tem dado correspondente")
+        print("Caso de Teste Dados 04 - Esse ID nao tem dado correspondente")
         Destroi_Dados()
         Cria_Dados()
         retorno_esperado = Muda_Status(7)
@@ -100,7 +101,7 @@ class Test(unittest.TestCase):
         self.assertEqual(retorno_esperado,1)
 
     def testDados_09_JogarDados_Todos_Dados_Congelados(self): 
-        print("Caso de Teste Dados 09 - Todos os dados estão congelados")
+        print("Caso de Teste Dados 09 - Todos os dados estao congelados")
         Destroi_Dados()
         Cria_Dados()
         #Mudar o status de todos para ficarem "congelado": True
@@ -143,103 +144,126 @@ class Test(unittest.TestCase):
 
     def testRodada_02_CriaRodada_Numero_Maximo_Atingido(self): 
         print("Caso de Teste Rodada 02 - O Numero de rodadas do jogo ja foi atingido")
+        Deleta_Rodadas()
+        for _ in range(27):
+            Cria_Rodada()
         retorno_esperado = Cria_Rodada()
         self.assertEqual(retorno_esperado,1)
         
     def testRodada_03_Verifica_Tentativa_Ok_Condicao_Retorno(self): 
         print("Caso de Teste Rodada 03 - Ainda existe tentativa disponivel")
+        Deleta_Rodadas()
+        Cria_Rodada()
         retorno_esperado = Verifica_Tentativa()
         self.assertEqual(retorno_esperado,0)
 
     def testRodada_04_Verifica_Tentativa_Nao_Existem_Tentativas(self): 
-        print("Caso de Teste Rodada 04 -Não existe tentativa disponivel")
-        Atualiza_Tentativas(0)   #Considerando que essa função seta para 0 o num de tentativas
+        print("Caso de Teste Rodada 04 -Nao existe tentativa disponivel")
+        Deleta_Rodadas()
+        Cria_Rodada()
+        retorno = Atualiza_Tentativas(0)
+        print('retorno da atualiza tentativas: ', retorno)
         retorno_esperado = Verifica_Tentativa()
         self.assertEqual(retorno_esperado,1)
 
     def testRodada_05_Verifica_Tentativa_Nao_Existe_Rodada(self): 
         print("Caso de Teste Rodada 05 - Nao existe rodada criada")
-        Destruir_Rodadas()   #Tem que adicionar na documentação
+        Deleta_Rodadas()
         retorno_esperado = Verifica_Tentativa()
         self.assertEqual(retorno_esperado,2)
 
     def testRodada_06_Atualiza_Tentativa_Ok_Condicao_Retorno(self): 
         print("Caso de Teste Rodada 06 - Suceso, tentativa atualizada")
+        Deleta_Rodadas()
         Cria_Rodada()
         retorno_esperado = Atualiza_Tentativas(2)
         self.assertEqual(retorno_esperado,0)
         
-    def testRodada_07_Atualiza_Tentativa_Ok_Condicao_Retorno(self): 
+    def testRodada_07_Atualiza_Tentativa_Numero_Inconsistente_Condicao_Retorno(self): 
         print("Caso de Teste Rodada 07 - Suceso, tentativa atualizada")
+        Deleta_Rodadas()
+        Cria_Rodada()
         retorno_esperado = Atualiza_Tentativas(3)
         self.assertEqual(retorno_esperado,1)
 
     def testRodada_08_Atualiza_Tentativa_NTentativas_Diferente(self): 
-        print("Caso de Teste Rodada 08 - O numero de tentativas no paramentro é diferente de tentativas -1 do ultimo objeto")
+        print("Caso de Teste Rodada 08 - O numero de tentativas no paramentro eh diferente de tentativas -1 do ultimo objeto")
+        Deleta_Rodadas()
         retorno_esperado = Atualiza_Tentativas(10)
         self.assertEqual(retorno_esperado,2)
 
     def testRodada_09_Atualiza_Tentativa_Parametro_NaoAceito(self): 
-        print("Caso de Teste Rodada 09 - Tentativa no parammetro não condiz com o range aceito")
+        print("Caso de Teste Rodada 09 - Tentativa no parammetro nao condiz com o range aceito")
+        Deleta_Rodadas()
         retorno_esperado = Atualiza_Tentativas("1")
         self.assertEqual(retorno_esperado,3)
 
     def testRodada_10_Atualiza_Tentativa_Nao_Existe_RodadaCriada(self): 
         print("Caso de Teste Rodada 10 -Nao existe rodada criada")
-        Destruir_Rodadas()   #Tem que adicionar na documentação
+        Deleta_Rodadas()
         retorno_esperado = Atualiza_Tentativas(2)
         self.assertEqual(retorno_esperado,4)
 
     def testRodada_11_ModificaDadosRodada_Ok_Condicao_Retorno(self): 
         print("Caso de Teste Rodada 11 - Sucesso, objeto rodada atualizado")
+        Deleta_Rodadas()
         Cria_Rodada()
         retorno_esperado = Modifica_Dados_Rodada()
+        Destroi_Dados()
         self.assertEqual(retorno_esperado,0)
 
     def testRodada_12_ModificaDadosRodada_Sem_Tentativas_Sobrando(self): 
         print("Caso de Teste Rodada 12 -Nao tem tentivas sobrando")
+        Deleta_Rodadas()
+        Cria_Rodada()
         Atualiza_Tentativas(0)
         retorno_esperado = Modifica_Dados_Rodada()
+        Destroi_Dados()
         self.assertEqual(retorno_esperado,1)
 
     def testRodada_13_ModificaDadosRodada_Dados_Congelados(self): 
-        print("Caso de Teste Rodada 13 -Dados estão Congelados")
-        Cria_Rodada()   #Rodada foi criada pois a anterior nao tem tentativa sobrando
-        #Dados ja foram criados em Mostra_Dados nao jogados
+        print("Caso de Teste Rodada 13 -Dados estao Congelados")
+        Deleta_Rodadas()
+        Cria_Rodada()
+        Cria_Dados()
         Muda_Status(1)
         Muda_Status(2)
         Muda_Status(3)
         Muda_Status(4)
         Muda_Status(5)
         retorno_esperado = Modifica_Dados_Rodada()
+        Destroi_Dados()
         self.assertEqual(retorno_esperado,2)
         
     def testRodada_14_ModificaDadosRodada_Nao_Existe_Dado(self): 
-        print("Caso de Teste Rodada 14 -Não existem dados")
-        Destruir_Dados()
+        print("Caso de Teste Rodada 14 -Nao existem dados")
+        Deleta_Rodadas()
+        Cria_Rodada()
         retorno_esperado = Modifica_Dados_Rodada()
+        Destroi_Dados()
         self.assertEqual(retorno_esperado,3)
         
-    def testRodada_15_ModificaDadosRodada_Não_Existe_Rodada(self): 
-        print("Caso de Teste Rodada 15 - Não existe rodada criada")
-        Destruir_Rodadas()
+    def testRodada_15_ModificaDadosRodada_Nao_Existe_Rodada(self): 
+        print("Caso de Teste Rodada 15 - Nao existe rodada criada")
+        Deleta_Rodadas()
         retorno_esperado = Modifica_Dados_Rodada()
+        Destroi_Dados()
         self.assertEqual(retorno_esperado,4)
 
     def testRodada_16_PegaRodada_Ok_Condicao_Retorno(self): 
         print("Caso de Teste Rodada 16 - Sucesso")
-        Cria_Dados()
+        Deleta_Rodadas()
         Cria_Rodada()
         retorno_esperado = Pega_Rodada()
-        self.assertEqual(retorno_esperado,{0:{"Tentativas":2,"dados_rodada": [1, 2, 3,4,5]}})
+        self.assertEqual(retorno_esperado,{0: { 1: {"tentativas":3 ,"dados_rodada": []} }})
         
     def testRodada_17_PegaRodada_Lista_Rodadas_Vazia(self): 
         print("Caso de Teste Rodada 17 - Lista de Rodadas Vazia")
-        Destruir_Rodadas()
+        Deleta_Rodadas()
         retorno_esperado = Pega_Rodada()
-        self.assertEqual(retorno_esperado,{1:NULL})
+        self.assertEqual(retorno_esperado,{1:None})
 
-##########################################################################
+    ##########################################################################
                     #Modulo Jogo
 
     def testJogo_01_CriaNovoJogo_Ok_Condicao_Retorno(self): 
@@ -349,6 +373,185 @@ class Test(unittest.TestCase):
         JogadorAtual = {3: "Carlos"}
         retorno_esperado = Tipo_Pontuacao(Dados,JogadorAtual)
         self.assertEqual(retorno_esperado,{4: []})  
+
+##########################################################################
+                    #Modulo Tabuleiro
+
+    def testTabuleiro_01_CriarTab_Ok_Condicao_Retorno(self): 
+        print("Caso de Teste Tabuleiro 01- Tabuleiro inicializado com sucesso")
+        retorno_esperado = Cria_Tab()
+        self.assertEqual(retorno_esperado,0)
         
+    def testTabuleiro_02_CriarTab_Ja_Existe_Tabuleiro(self): 
+        print("Caso de Teste Tabuleiro 02 - Tabuleiro ja existe")
+        retorno_esperado = Criar_Tab()
+        self.assertEqual(retorno_esperado,1)
+
+    def testTabuleiro_03_DestruirTab_Ok_Condicao_Retorno(self): 
+        print("Caso de Teste Tabuleiro 03 - Tabuleiro destruido com sucesso")
+        retorno_esperado = Destruir_Tab()
+        self.assertEqual(retorno_esperado,0)
+
+    def testTabuleiro_04_DestruirTab_Nao_Existe_Tab(self): 
+        print("Caso de Teste Tabuleiro 04 - Tabuleiro nao existe")
+        retorno_esperado = Destruir_Tab()
+        self.assertEqual(retorno_esperado,1)
+        
+    def testTabuleiro_05_InserirPontuacao_Ok_Condicao_Retorno(self): 
+        print("Caso de Teste Tabuleiro 05 - Pontuacao Inserida com sucesso")
+        Cria_Tab()
+        jogadorAtual = {2: "Carol"}
+        pontuação_atual = 20
+        retorno_esperado = InserirPontuação(pontuação_atual, jogadorAtual)
+        self.assertEqual(retorno_esperado,{0:listaPont})# nao sei retornar a lista
+        
+    def testTabuleiro_06_InserirPontuacao_PontuacaoAtual_Nao_Int(self): 
+        print("Caso de Teste Tabuleiro 06 - Pontuacao Atual nao é INT")
+        jogadorAtual = {2: "Carol"}
+        pontuação_atual = "Quina" 
+        retorno_esperado = InserirPontuação(pontuação_atual, jogadorAtual)
+        self.assertEqual(retorno_esperado,{1:[]})
+        
+    def testTabuleiro_07_InserirPontuacao_JogadorAtual_Nao_Objeto(self): 
+        print("Caso de Teste Tabuleiro 07 - Jogador Atual não é objeto")
+        jogadorAtual = "nao Jogador"
+        pontuação_atual = 20            
+        retorno_esperado = InserirPontuação(pontuação_atual, jogadorAtual)
+        #tem que colocar aqui como retorno esperado o inserir pontuacao? ou só atualiza
+        #tem que colocar só a InserirPontuação
+        self.assertEqual(retorno_esperado,{2:[]})
+        
+    def testTabuleiro_08_InserirPontuacao_JogadorAtual_Nao_Está_Jogo(self): 
+        print("Caso de Teste Tabuleiro 08 - Jogador Atual não está no Jogo")
+        jogadorAtual = {3: "Carlos"}
+        jogadores = [{1: "Aiko"}, {2: "Carol"}]
+        pontuação_atual = 20  
+        retorno_esperado = InserirPontuação(pontuação_atual, jogadorAtual)
+        self.assertEqual(retorno_esperado,{3:[]})
+
+    def testTabuleiro_09_VerificaVencedor_Ok_Condicao_Retorno(self): 
+        print("Caso de Teste Tabuleiro 09 - Verificação feita com sucesso")
+        Destruir_Tab()
+        #Nao sei se é necessario, mas vou colocar todas as pontuações para preencher o tabuleiro
+        Cria_Tab()
+        jogadorAtual = {1: "Aiko"}
+        pontuação_atual = 5 #5 1's
+        InserirPontuação(pontuação_atual, jogadorAtual)
+        jogadorAtual = {2: "Carol"}
+        pontuação_atual = 4 #4 1's 
+        InserirPontuação(pontuação_atual, jogadorAtual)
+        jogadorAtual = {1: "Aiko"}
+        pontuação_atual = 10 #5 2's
+        InserirPontuação(pontuação_atual, jogadorAtual)
+        jogadorAtual = {2: "Carol"}
+        pontuação_atual = 10 #5 2's 
+        InserirPontuação(pontuação_atual, jogadorAtual)
+        jogadorAtual = {1: "Aiko"}
+        pontuação_atual = 15 #5 3's
+        InserirPontuação(pontuação_atual, jogadorAtual)
+        jogadorAtual = {2: "Carol"}
+        pontuação_atual = 15 #5 3's 
+        InserirPontuação(pontuação_atual, jogadorAtual)
+        jogadorAtual = {1: "Aiko"}
+        pontuação_atual = 20 #5 4's
+        InserirPontuação(pontuação_atual, jogadorAtual)
+        jogadorAtual = {2: "Carol"}
+        pontuação_atual = 20 #5 4's 
+        InserirPontuação(pontuação_atual, jogadorAtual)
+        jogadorAtual = {1: "Aiko"}
+        pontuação_atual = 25 #5 5's
+        InserirPontuação(pontuação_atual, jogadorAtual)
+        jogadorAtual = {2: "Carol"}
+        pontuação_atual = 25 #5 5's 
+        InserirPontuação(pontuação_atual, jogadorAtual)
+        jogadorAtual = {1: "Aiko"}
+        pontuação_atual = 30 #5 6's
+        InserirPontuação(pontuação_atual, jogadorAtual)
+        jogadorAtual = {2: "Carol"}
+        pontuação_atual = 30 #5 6's 
+        InserirPontuação(pontuação_atual, jogadorAtual)
+        jogadorAtual = {1: "Aiko"}
+        pontuação_atual = 19 #3 iguais de 5 e 2 de 2 "Tres de um tipo"
+        InserirPontuação(pontuação_atual, jogadorAtual)
+        jogadorAtual = {2: "Carol"}
+        pontuação_atual = 17 #3 iguais de 5 e 2 de 1 "Tres de um tipo"
+        InserirPontuação(pontuação_atual, jogadorAtual)
+        jogadorAtual = {1: "Aiko"}
+        pontuação_atual = 24 #4 iguais de 5 e 1 de 4 "Quatro de um tipo"
+        InserirPontuação(pontuação_atual, jogadorAtual)
+        jogadorAtual = {2: "Carol"}
+        pontuação_atual = 23 #4 iguais de 5 e 1 de 3 "Quatro de um tipo"
+        InserirPontuação(pontuação_atual, jogadorAtual)
+        jogadorAtual = {1: "Aiko"}
+        pontuação_atual = 25 #Full House
+        InserirPontuação(pontuação_atual, jogadorAtual)
+        jogadorAtual = {2: "Carol"}
+        pontuação_atual = 25 #Full House
+        InserirPontuação(pontuação_atual, jogadorAtual)
+        jogadorAtual = {1: "Aiko"}
+        pontuação_atual = 30 #Sequencia Baixa
+        InserirPontuação(pontuação_atual, jogadorAtual)
+        jogadorAtual = {2: "Carol"}
+        pontuação_atual = 30 #Sequencia Baixa
+        InserirPontuação(pontuação_atual, jogadorAtual)
+        jogadorAtual = {1: "Aiko"}
+        pontuação_atual = 40 #Sequencia Alta
+        InserirPontuação(pontuação_atual, jogadorAtual)
+        jogadorAtual = {2: "Carol"}
+        pontuação_atual = 40 #Sequencia Alta
+        InserirPontuação(pontuação_atual, jogadorAtual)
+        jogadorAtual = {1: "Aiko"}
+        pontuação_atual = 50 #YAHTZEE
+        InserirPontuação(pontuação_atual, jogadorAtual)
+        jogadorAtual = {2: "Carol"}
+        pontuação_atual = 50 #YAHTZEE
+        InserirPontuação(pontuação_atual, jogadorAtual)
+        jogadorAtual = {1: "Aiko"}
+        pontuação_atual = 0 #Chance
+        InserirPontuação(pontuação_atual, jogadorAtual)
+        jogadorAtual = {2: "Carol"}
+        pontuação_atual = 0 #Chance
+        InserirPontuação(pontuação_atual, jogadorAtual)
+        retorno_esperado = Verifica_Vencedor()
+        self.assertEqual(retorno_esperado,{0: 1})
+
+    def testTabuleiro_09_VerificaVencedor_Ok_Condicao_Retorno(self): 
+        print("Caso de Teste Tabuleiro 09 - Verificação feita com sucesso")
+        Destruir_Tab()
+        Cria_Tab()
+        retorno_esperado = Verifica_Vencedor()
+        self.assertEqual(retorno_esperado,{1: NULL})
+
+    def testTabuleiro_09_VerificaVencedor_Ok_Condicao_Retorno(self): 
+        print("Caso de Teste Tabuleiro 09 - Verificação feita com sucesso")
+        Destruir_Tab()
+        retorno_esperado = Verifica_Vencedor()
+        self.assertEqual(retorno_esperado,{2: NULL})
+        
+##########################################################################
+                    #Modulo Principal
+    
+    def testPrincipal_01_TelaInicial_Ok_Condicao_Retorno(self): 
+        print("Caso de Teste Principal 01- Tela Inicial criada com sucesso")
+        retorno_esperado = Tela_Inicial()
+        self.assertEqual(retorno_esperado,0)
+        
+    def testPrincipal_02_TelaInicial_Jogo_Criado(self): 
+        print("Caso de Teste Principal 02- Já existe jogo criado")
+        Cria_Novo_Jogo()
+        retorno_esperado = Tela_Inicial()
+        self.assertEqual(retorno_esperado,1)
+
+    def testPrincipal_03_DesenhaTab_Ok_Condicao_Retorno(self): 
+        print("Caso de Teste Principal 03- Desenho do Tabuleiro criada com sucesso")
+        Cria_Tab()
+        retorno_esperado = Desenha_Tab()
+        self.assertEqual(retorno_esperado,0)
+
+    def testPrincipal_04_TelaInicial_Nao_Existe_Tab(self): 
+        print("Caso de Teste Principal 04- Nao existe tabuleiro pra ser desenhado")
+        Destruir_Tab()
+        retorno_esperado =Desenha_Tab()
+        self.assertEqual(retorno_esperado,1)
 
 unittest.main()
