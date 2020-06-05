95a1aad7e9fb62e93fcbd194c524106c4ee1b6aa
Carlos Ribeiro <carlosagente003@gmail.com>
2020-06-05 17:49:17 -0300

    Principal atualizado para mostrar as pontuacoes

--- Principal.py
+++ Principal.py
@@ -197,44 +197,162 @@ def Pontuacoes_Disponiveis(dados,idJogadorAtual):
     
     if tabuleiro[0][idJogadorAtual-1] == None:
         button_Ones = Button(root, text="Ones",bg="green",fg="white",padx = 20,pady =10, command=lambda: Insere(dados,"Ones",idJogadorAtual))
+
+        pontuacao = Pontuacao.Calcula_Pontuacao(dados,"Ones", idJogadorAtual)[0]
+        if idJogadorAtual == 1:
+            label_Ones1 = Label(root, text=pontuacao,borderwidth=2,relief="groove",padx=20,pady=10, bg = "white",fg="red")
+            label_Ones1.grid(row=2,column=1,sticky=N+S+E+W)
+        elif idJogadorAtual == 2:
+            label_Ones2 = Label(root, text=pontuacao,borderwidth=2,relief="groove",padx=20,pady=10, bg = "white",fg="red")
+            label_Ones2.grid(row=2,column=2,sticky=N+S+E+W)
+        
         button_Ones.grid(row=2,column=0,sticky=W+E)
     if tabuleiro[1][idJogadorAtual-1] == None:
         button_Twos = Button(root, text="Twos",bg="green",fg="white",padx = 20,pady =10, command=lambda: Insere(dados,"Twos",idJogadorAtual))
+
+        pontuacao = Pontuacao.Calcula_Pontuacao(dados,"Twos", idJogadorAtual)[0]
+        if idJogadorAtual == 1:
+            label_Twos1 = Label(root, text=pontuacao,borderwidth=2,relief="groove",padx=20,pady=10, bg = "white",fg="red")
+            label_Twos1.grid(row=3,column=1,sticky=N+S+E+W)
+        elif idJogadorAtual == 2:
+            label_Twos2 = Label(root, text=pontuacao,borderwidth=2,relief="groove",padx=20,pady=10, bg = "white",fg="red")
+            label_Twos2.grid(row=3,column=2,sticky=N+S+E+W)
+        
         button_Twos.grid(row=3,column=0,sticky=W+E)
     if tabuleiro[2][idJogadorAtual-1] == None:
         button_Threes = Button(root, text="Threes",bg="green",fg="white",padx = 20,pady =10, command=lambda: Insere(dados,"Threes",idJogadorAtual))
+
+        pontuacao = Pontuacao.Calcula_Pontuacao(dados,"Threes", idJogadorAtual)[0]
+        if idJogadorAtual == 1:
+            label_Threes1 = Label(root, text=pontuacao,borderwidth=2,relief="groove",padx=20,pady=10, bg = "white",fg="red")
+            label_Threes1.grid(row=4,column=1,sticky=N+S+E+W)
+        elif idJogadorAtual == 2:
+            label_Threes2 = Label(root, text=pontuacao,borderwidth=2,relief="groove",padx=20,pady=10, bg = "white",fg="red")
+            label_Threes2.grid(row=4,column=2,sticky=N+S+E+W)
+        
         button_Threes.grid(row=4,column=0,sticky=W+E)
     if tabuleiro[3][idJogadorAtual-1] == None:
         button_Fours = Button(root, text="Fours",bg="green",fg="white",padx = 20,pady =10, command=lambda: Insere(dados,"Fours",idJogadorAtual))
+
+        pontuacao = Pontuacao.Calcula_Pontuacao(dados,"Fours", idJogadorAtual)[0]
+        if idJogadorAtual == 1:
+            label_Fours1 = Label(root, text=pontuacao,borderwidth=2,relief="groove",padx=20,pady=10, bg = "white",fg="red")
+            label_Fours1.grid(row=5,column=1,sticky=N+S+E+W)
+        elif idJogadorAtual == 2:
+            label_Fours2 = Label(root, text=pontuacao,borderwidth=2,relief="groove",padx=20,pady=10, bg = "white",fg="red")
+            label_Fours2.grid(row=5,column=2,sticky=N+S+E+W)
+        
         button_Fours.grid(row=5,column=0,sticky=W+E)
     if tabuleiro[4][idJogadorAtual-1] == None:
         button_Fives = Button(root, text="Fives",bg="green",fg="white",padx = 20,pady =10, command=lambda: Insere(dados,"Fives",idJogadorAtual))
+
+        pontuacao = Pontuacao.Calcula_Pontuacao(dados,"Fives", idJogadorAtual)[0]
+        if idJogadorAtual == 1:
+            label_Fives1 = Label(root, text=pontuacao,borderwidth=2,relief="groove",padx=20,pady=10, bg = "white",fg="red")
+            label_Fives1.grid(row=6,column=1,sticky=N+S+E+W)
+        elif idJogadorAtual == 2:
+            label_Fives2 = Label(root, text=pontuacao,borderwidth=2,relief="groove",padx=20,pady=10, bg = "white",fg="red")
+            label_Fives2.grid(row=6,column=2,sticky=N+S+E+W)
+        
         button_Fives.grid(row=6,column=0,sticky=W+E)
     if tabuleiro[5][idJogadorAtual-1] == None:
         button_Sixes = Button(root, text="Sixes",bg="green",fg="white",padx = 20,pady =10, command=lambda: Insere(dados,"Sixes",idJogadorAtual))
+
+        pontuacao = Pontuacao.Calcula_Pontuacao(dados,"Sixes", idJogadorAtual)[0]
+        if idJogadorAtual == 1:
+            label_Sixes1 = Label(root, text=pontuacao,borderwidth=2,relief="groove",padx=20,pady=10, bg = "white",fg="red")
+            label_Sixes1.grid(row=7,column=1,sticky=N+S+E+W)
+        elif idJogadorAtual == 2:
+            label_Sixes2 = Label(root, text=pontuacao,borderwidth=2,relief="groove",padx=20,pady=10, bg = "white",fg="red")
+            label_Sixes2.grid(row=7,column=2,sticky=N+S+E+W)
+        
         button_Sixes.grid(row=7,column=0,sticky=W+E)
     if tabuleiro[6][idJogadorAtual-1] == None:
         button_ThreeOfAKind = Button(root, text="Three of a Kind",bg="green",fg="white",padx = 20,pady =10, command=lambda: Insere(dados,"Three of a Kind",idJogadorAtual))
+
+        pontuacao = Pontuacao.Calcula_Pontuacao(dados,"Three of a Kind", idJogadorAtual)[0]
+        if idJogadorAtual == 1:
+            label_ThreeOfAKind1 = Label(root, text=pontuacao,borderwidth=2,relief="groove",padx=20,pady=10, bg = "white",fg="red")
+            label_ThreeOfAKind1.grid(row=8,column=1,sticky=N+S+E+W)
+        elif idJogadorAtual == 2:
+            label_ThreeOfAKind2 = Label(root, text=pontuacao,borderwidth=2,relief="groove",padx=20,pady=10, bg = "white",fg="red")
+            label_ThreeOfAKind2.grid(row=8,column=2,sticky=N+S+E+W)
+        
         button_ThreeOfAKind.grid(row=8,column=0,sticky=W+E)
     if tabuleiro[7][idJogadorAtual-1] == None:
         button_FourOfAKind = Button(root, text="Four of a Kind",bg="green",fg="white",padx = 20,pady =10, command=lambda: Insere(dados,"Four of a Kind",idJogadorAtual))
+
+        pontuacao = Pontuacao.Calcula_Pontuacao(dados,"Four of a Kind", idJogadorAtual)[0]
+        if idJogadorAtual == 1:
+            label_FourOfAKind1 = Label(root, text=pontuacao,borderwidth=2,relief="groove",padx=20,pady=10, bg = "white",fg="red")
+            label_FourOfAKind1.grid(row=9,column=1,sticky=N+S+E+W)
+        elif idJogadorAtual == 2:
+            label_FourOfAKind2 = Label(root, text=pontuacao,borderwidth=2,relief="groove",padx=20,pady=10, bg = "white",fg="red")
+            label_FourOfAKind2.grid(row=9,column=2,sticky=N+S+E+W)
+        
         button_FourOfAKind.grid(row=9,column=0,sticky=W+E)
     if tabuleiro[8][idJogadorAtual-1] == None:
         button_FullHouse = Button(root, text="Full House",bg="green",fg="white",padx = 20,pady =10, command=lambda: Insere(dados,"Full House",idJogadorAtual))
+
+        pontuacao = Pontuacao.Calcula_Pontuacao(dados,"Full House", idJogadorAtual)[0]
+        if idJogadorAtual == 1:
+            label_FullHouse1 = Label(root, text=pontuacao,borderwidth=2,relief="groove",padx=20,pady=10, bg = "white",fg="red")
+            label_FullHouse1.grid(row=10,column=1,sticky=N+S+E+W)
+        elif idJogadorAtual == 2:
+            label_FullHouse2 = Label(root, text=pontuacao,borderwidth=2,relief="groove",padx=20,pady=10, bg = "white",fg="red")
+            label_FullHouse2.grid(row=10,column=2,sticky=N+S+E+W)
+        
         button_FullHouse.grid(row=10,column=0,sticky=W+E)
     if tabuleiro[9][idJogadorAtual-1] == None:
         button_SmallStraight = Button(root, text="Small Straight",bg="green",fg="white",padx = 20,pady =10, command=lambda: Insere(dados,"Small Straight",idJogadorAtual))
+
+        pontuacao = Pontuacao.Calcula_Pontuacao(dados,"Small Straight", idJogadorAtual)[0]
+        if idJogadorAtual == 1:
+            label_SmallStraight1 = Label(root, text=pontuacao,borderwidth=2,relief="groove",padx=20,pady=10, bg = "white",fg="red")
+            label_SmallStraight1.grid(row=11,column=1,sticky=N+S+E+W)
+        elif idJogadorAtual == 2:
+            label_SmallStraight2 = Label(root, text=pontuacao,borderwidth=2,relief="groove",padx=20,pady=10, bg = "white",fg="red")
+            label_SmallStraight2.grid(row=11,column=2,sticky=N+S+E+W)
+        
         button_SmallStraight.grid(row=11,column=0,sticky=W+E)   
     if tabuleiro[10][idJogadorAtual-1] == None:
         button_LargeStraight = Button(root, text="Large Straight",bg="green",fg="white",padx = 20,pady =10, command=lambda: Insere(dados,"Large Straight",idJogadorAtual))
+
+        pontuacao = Pontuacao.Calcula_Pontuacao(dados,"Large Straight", idJogadorAtual)[0]
+        if idJogadorAtual == 1:
+            label_LargeStraight1 = Label(root, text=pontuacao,borderwidth=2,relief="groove",padx=20,pady=10, bg = "white",fg="red")
+            label_LargeStraight1.grid(row=12,column=1,sticky=N+S+E+W)
+        elif idJogadorAtual == 2:
+            label_LargeStraight2 = Label(root, text=pontuacao,borderwidth=2,relief="groove",padx=20,pady=10, bg = "white",fg="red")
+            label_LargeStraight2.grid(row=12,column=2,sticky=N+S+E+W)
+        
         button_LargeStraight.grid(row=12,column=0,sticky=W+E)
     if tabuleiro[11][idJogadorAtual-1] == None:
         button_Chance = Button(root, text="Chance",bg="green",fg="white",padx = 20,pady =10, command=lambda: Insere(dados,"Chance",idJogadorAtual))
+
+        pontuacao = Pontuacao.Calcula_Pontuacao(dados,"Chance", idJogadorAtual)[0]
+        if idJogadorAtual == 1:
+            label_Chance1 = Label(root, text=pontuacao,borderwidth=2,relief="groove",padx=20,pady=10, bg = "white",fg="red")
+            label_Chance1.grid(row=13,column=1,sticky=N+S+E+W)
+        elif idJogadorAtual == 2:
+            label_Chance2 = Label(root, text=pontuacao,borderwidth=2,relief="groove",padx=20,pady=10, bg = "white",fg="red")
+            label_Chance2.grid(row=13,column=2,sticky=N+S+E+W)
+        
         button_Chance.grid(row=13,column=0,sticky=W+E)
     if tabuleiro[12][idJogadorAtual-1] == None:
         button_Yahtzee = Button(root, text="Yahtzee",bg="green",fg="white",padx = 20,pady =10, command=lambda: Insere(dados,"Yahtzee",idJogadorAtual))
+
+        pontuacao = Pontuacao.Calcula_Pontuacao(dados,"Yahtzee", idJogadorAtual)[0]
+        if idJogadorAtual == 1:
+            label_Yahtzee1 = Label(root, text=pontuacao,borderwidth=2,relief="groove",padx=20,pady=10, bg = "white",fg="red")
+            label_Yahtzee1.grid(row=14,column=1,sticky=N+S+E+W)
+        elif idJogadorAtual == 2:
+            label_Yahtzee2 = Label(root, text=pontuacao,borderwidth=2,relief="groove",padx=20,pady=10, bg = "white",fg="red")
+            label_Yahtzee2.grid(row=14,column=2,sticky=N+S+E+W)
+        
         button_Yahtzee.grid(row=14,column=0,sticky=W+E)
     return
+
 
 def Dado_clicado(dados,butao,numDado):
     
@@ -332,11 +450,11 @@ def Desenha_Dados(dados,idJogadorAtual):
     elif listaFaces[4] == 6:
         button_Dado5 = Button(root,image = imgDado6,bg = 'green',activebackground='red',command=lambda: Dado_clicado(dados,button_Dado5,5))
         
-    button_Dado1.grid(row=15,column=0,columnspan=2,sticky=W)
-    button_Dado2.grid(row=15,column=0,columnspan=2,sticky=N)
-    button_Dado3.grid(row=15,column=1,columnspan=2,sticky=W)
-    button_Dado4.grid(row=15,column=1,columnspan=2,sticky=N)
-    button_Dado5.grid(row=15,column=2,sticky=E)
+    button_Dado1.grid(row=16,column=0,columnspan=2,sticky=W)
+    button_Dado2.grid(row=16,column=0,columnspan=2,sticky=N)
+    button_Dado3.grid(row=16,column=1,columnspan=2,sticky=W)
+    button_Dado4.grid(row=16,column=1,columnspan=2,sticky=N)
+    button_Dado5.grid(row=16,column=2,sticky=E)
 
     #Desenha_Tab(dados)
 
@@ -406,6 +524,13 @@ def Desenha_Tab(dados,idJogadorAtual):
     label_Yahtzee1 = Label(root, text=pontuacao,borderwidth=2,relief="groove",padx=20,pady=10, bg = "white")
     pontuacao = tabuleiro[12][1]
     label_Yahtzee2 = Label(root, text=pontuacao,borderwidth=2,relief="groove",padx=20,pady=10, bg = "white")
+
+    label_Total = Label(root, text="Total",bg="green",fg="black",relief="raised",padx = 20,pady =10)
+    
+    pontuacao = tabuleiro[13][0]
+    label_Total1 = Label(root, text=pontuacao,borderwidth=2,relief="groove",padx=20,pady=10, bg = "white")
+    pontuacao = tabuleiro[13][1]
+    label_Total2 = Label(root, text=pontuacao,borderwidth=2,relief="groove",padx=20,pady=10, bg = "white")
     
     labelJogador1.grid(row=1,column=1)
     labelJogador2.grid(row=1,column=2)
@@ -470,9 +595,13 @@ def Desenha_Tab(dados,idJogadorAtual):
     label_Yahtzee1.grid(row=14,column=1,sticky=N+S+E+W)
     label_Yahtzee2.grid(row=14,column=2,sticky=N+S+E+W)
 
+    label_Total.grid(row=15,column=0,sticky=W+E)
+    label_Total1.grid(row=15,column=1,sticky=W+E)
+    label_Total2.grid(row=15,column=2,sticky=W+E)
+
     #Desenha_Dados(dados)
 
-    button_JogarDados.grid(row=16,column=0,columnspan=3)
+    button_JogarDados.grid(row=17,column=0,columnspan=3,sticky=W+E)
     
     return {None}
 
